f9bd175b8865d49dfaef1448b7e5441ba32149da
sjoplin <sjoplin@lawn-128-61-61-33.lawn.gatech.edu>
2016-11-14 19:14:58 -0500

    downloaded standard assets for fps

---
+++ For Buddy!/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float sensitivity;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		float lookLeft = Input.GetAxis ("Mouse X");
+		float lookUp = Input.GetAxis ("Mouse Y");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+}
