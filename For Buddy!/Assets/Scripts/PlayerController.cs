32279f0d7203aede65236d04e9b6156fff18bf4a
sjoplin <sjoplin@lawn-128-61-61-33.lawn.gatech.edu>
2016-11-14 19:36:59 -0500

    removing the standard assets. Were doing everthing on our own

--- For Buddy!/Assets/Scripts/PlayerController.cs
+++ For Buddy!/Assets/Scripts/PlayerController.cs
@@ -8,11 +8,12 @@ public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb;
 
+	//gets the rigidbody
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
 	}
-
+	//Movement control
 	void FixedUpdate ()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
@@ -22,5 +23,7 @@ public class PlayerController : MonoBehaviour {
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce (movement * speed);
+		//Vector3 rotation = new Vector3 (0, lookLeft, 0);
+		//rb.AddTorque (rotation * sensitivity);
 	}
 }
